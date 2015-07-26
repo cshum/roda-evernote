495c5055c8a9541f1e1245aba5dfd41c20de3bc0
Adrian Shum <me@cshum.com>
2015-07-26 11:00:51 +0800

    ADD server.js, prepare test

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
-var test     = require('tape'),
-    rodabase = require('rodabase'),
-    evernote = require('../'),
+var rodabase = require('rodabase'),
+    evernote = require('./'),
+    express  = require('express'),
     rest     = require('roda-rest');
 
 var roda = rodabase('./db');
@@ -15,4 +15,4 @@ app.get('/api/sync/:token', function(req, res){
 });
 app.use('/api', rest(roda)); //roda rest api
 app.listen(3000);
-
+console.log('roda-evernote is listening on port '+ 3000);
